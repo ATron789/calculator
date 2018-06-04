4e3fa3c3b6d56821829bb365514644d95739ad99
ATron789 <albertopavano@gmail.com>
2018-06-04 22:31:40 +0100

    super messy awful code

--- calculator.rb
+++ calculator.rb
@@ -1,54 +1,45 @@
 require 'pry'
+class Calculator
 
-def get_int_values
-  [gets, gets].map{ |s| s.chomp.to_i }
+  attr_accessor :first_input, :second_input, :ops
+
+  def intialize (first_input, second_input, ops)
+
+    @first_input = first_input
+    @second_input = second_input
+    @ops = ops
+
+  end
+
+  def calculate
+    case ops
+    when 'sum'
+      return @first_input + @second_input
+    when 'min'
+      return  @first_input - @second_input
+    when 'tim'
+      return  @first_input * @second_input
+    when 'div'
+      return  @first_input / @second_input
+    end
+  end
+
 end
 
-puts "Would you like to [add], [multiply], or [subtract]?"
-response = gets.chomp
-
-case response.downcase
-when 'add'
-  puts "Which numbers would you like to add?"
-  operator = :+
-
-when 'subtract'
-  puts "Which numbers would you like to subtract?"
-  operator = :-
-
-when 'multiply'
-  puts "Which numbers would you like to multiply?"
-  operator = :*
-
-end
-
-answer = get_int_values.inject(operator)
-puts "The answer is... #{ answer }"
 
 
 
 
-# class Calculator
-#
-#   attr_accessor :first_input, :second_input, :ops
-#
-#   def intialize (first_input, second_input, ops)
-#
-#     @first_input = first_input
-#     @second_input = second_input
-#     @ops = ops
-#
-#   end
 #
 #   def operation
 #     case @ops
-#     when 'sum'
-#       puts @result = @first_input + @second_input
-#     when 'min'
-#       puts @result = @first_input - @second_input
-#     when 'tim'
-#       puts @result =  @first_input * @second_input
-#     when 'div'
-#       puts @result = @first_input / @second_input
-#     end
+    when 'sum'
+      puts @result = @first_input + @second_input
+    when 'min'
+      puts @result = @first_input - @second_input
+    when 'tim'
+      puts @result =  @first_input * @second_input
+    when 'div'
+      puts @result = @first_input / @second_input
+    end
 # end
--- myapp.rb
+++ myapp.rb
@@ -15,19 +15,22 @@ get '/ciao' do
   @first_input = params["first_input"].to_i
   @second_input = params["second_input"].to_i
   @ops = params["ops"]
-  @result = 0
 
-  #require_relative 'views/calculator'
-  case @ops
-  when 'sum'
-    @result = @first_input + @second_input
-  when 'min'
-    @result = @first_input - @second_input
-  when 'tim'
-    @result =  @first_input * @second_input
-  when 'div'
-    @result = @first_input / @second_input
-  end
-  
+  require_relative 'calculator'
+
+  @r = Calculator.new
+  @result = @r.calculate
+
+  # case @ops
+  # when 'sum'
+  #   @result = @first_input + @second_input
+  # when 'min'
+  #   @result = @first_input - @second_input
+  # when 'tim'
+  #   @result =  @first_input * @second_input
+  # when 'div'
+  #   @result = @first_input / @second_input
+  # end
+
   erb :ciao
 end
