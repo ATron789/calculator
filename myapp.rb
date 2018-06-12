ca9ca2f191d3a7cd49834cd84ce594ba40fd6e3d
ATron789 <albertopavano@gmail.com>
2018-06-12 22:33:10 +0100

    proto login

--- myapp.rb
+++ myapp.rb
@@ -2,22 +2,49 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
+Accounts = {
+  'user' => 'password'
+}
+
 get '/' do
-  @title = 'Maestro Calculator'
-  erb :index
+  @title = 'login'
+  erb :login
 end
+
+post '/home' do
+  @user = params['user']
+  @password = params['password']
+  binding.pry
+  if Accounts.key?(@user) && Accounts.has_value?(@password)
+    @title = 'Maestro Calculator'
+    erb :index
+  else
+    @title = 'register'
+    erb :account
+  end
+end
+
+post '/welcome' do
+  # binding.pry
+  @new_user = params['new_user']
+  @new_password = params['new_password']
+  Accounts[@new_user] = @new_password
+  erb :welcome
+end
+
+
 
 get '/ciao' do
 
   @title = 'Results'
-  @first_input = params["first_input"].to_f
-  @second_input = params["second_input"].to_f
-  @ops = params["ops"]
+  @fi = params["fi"].to_f
+  @si = params["si"].to_f
+  @op = params["op"]
 
   require './calculator'
 
-  @result = Calculator.new(@first_input, @second_input, @ops).calculate
-  # binding.pry
+  @result = Calculator.new(@fi, @si, @op).calculate
+  binding.pry
 
   erb :ciao
 end
