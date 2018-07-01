a85b8af4799a74d6a44298001aec165a176911bd
ATron789 <albertopavano@gmail.com>
2018-07-01 20:01:47 +0100

    rounded up

--- myapp.rb
+++ myapp.rb
@@ -3,36 +3,69 @@ require 'sinatra/reloader'
 require 'pry'
 
 Accounts = {
-  'user' => 'password'
+  'user' => 'pass'
 }
+@@current_user = nil
+@@login_attempts =  0
 
 get '/' do
-  @title = 'login'
-  erb :login
-end
-
-post '/home' do
-  @user = params['user']
-  @password = params['password']
-  binding.pry
-  if Accounts.key?(@user) && Accounts.has_value?(@password)
-    @title = 'Maestro Calculator'
-    erb :index
+  if @@current_user
+    redirect '/home'
   else
-    @title = 'register'
-    erb :account
+    redirect '/login'
   end
 end
 
-post '/welcome' do
+get '/login' do
+  @title = 'Login'
+  erb :login
+end
+
+get '/home' do
+  if @@current_user
+    @title = 'Maestro Calculator'
+    erb :index
+  else
+    redirect '/login'
+  end
+end
+
+post '/login' do
+  @user = params['user']
+  @password = params['password']
+  if Accounts.key?(@user) && Accounts.has_value?(@password)
+    @@login_attempts = 0
+    @@current_user = @user
+    redirect '/home'
+  else
+    @@login_attempts += 1
+    redirect '/login'
+  end
+end
+
+get '/registration' do
+  @title = 'Register your account'
+  erb :registration
+end
+
+post '/account_creation' do
   # binding.pry
-  @new_user = params['new_user']
+  @@login_attempts = 0
+  @@current_user = params['new_user']
   @new_password = params['new_password']
-  Accounts[@new_user] = @new_password
+  Accounts[@@current_user] = @new_password
+  redirect '/welcome'
+end
+
+get '/welcome' do
+  @current_user = @@current_user
   erb :welcome
 end
 
-
+post '/signout' do
+  @@current_user = nil
+  redirect '/login'
+end
 
 get '/ciao' do
 
@@ -44,7 +77,6 @@ get '/ciao' do
   require './calculator'
 
   @result = Calculator.new(@fi, @si, @op).calculate
-  binding.pry
 
   erb :ciao
 end
